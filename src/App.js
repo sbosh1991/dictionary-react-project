f5260ce5a9002b438e373439801fb7dba81061e2
Sbosh <82455037+sbosh1991@users.noreply.github.com>
2021-12-08 11:15:15 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      Hello world
+      <div>
+        <button className="btn btn-secondary"> Search</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
