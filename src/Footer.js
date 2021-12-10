3111fb418f96f04927c71ddadf7593fc4b8e064a
Sbosh <82455037+sbosh1991@users.noreply.github.com>
2021-12-10 14:04:33 +0100

    fixed footer url

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,13 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <footer>This was coded by Sbosh and is open-sourced on Github</footer>
+      <footer>
+        This was coded by{" "}
+        <a href="https://github.com/sbosh1991/dictionary-react-project">
+          Sbosh{" "}
+        </a>
+        and is open-sourced on Github
+      </footer>
     </div>
   );
 }
